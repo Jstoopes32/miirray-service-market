7b69a75e854b019fa8f95149a879a43c4b959b0a
Jstoopes32 <JanaiStoopes@gmail.com>
2025-05-04 17:28:44 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#ffffff',
+        gold: '#FFD700',
+      },
+    },
+  },
+  plugins: [],
+}
